e1209967971b5310e8d49ad98faab1e3fcf3fd28
Yunus Seyyar <mfyunuseyyar@gmail.com>
2024-12-08 02:51:14 +0300

    update

--- database/migrations/2024_12_05_123833_create_todos_table.php
+++ database/migrations/2024_12_05_123833_create_todos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->softDeletes();
+            $table->softDeletes("delete_at");
             $table->boolean("completed")->default(  false);
             $table->timestamps();
         });
